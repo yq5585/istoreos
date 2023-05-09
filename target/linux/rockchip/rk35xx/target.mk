3ead381d6f0282661f1ec6a13c80a5f772807baa
Liangbin Lian <jjm2473@gmail.com>
2023-05-09 13:35:54 +0800

    target/rockchip/rk35xx: update network smp affinity

--- target/linux/rockchip/rk35xx/target.mk
+++ target/linux/rockchip/rk35xx/target.mk
@@ -2,7 +2,7 @@ ARCH:=aarch64
 CPU_TYPE:=cortex-a53
 BOARDNAME:=RK3568/RK3588 boards (64 bit)
 
-DEFAULT_PACKAGES+=kmod-r8125
+DEFAULT_PACKAGES+=kmod-r8125 ethtool
 
 define Target/Description
 	Build firmware image for Rockchip RK3568/RK3588 devices.
